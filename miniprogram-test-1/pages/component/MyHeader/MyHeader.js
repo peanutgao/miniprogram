fc7726432c7fcec7c4828bd782f5f8e153d537a3
Joseph <Joseph0750@gmail.com>
2019-04-26 22:04:56 +0800

    自定义组件

---
+++ miniprogram-test-1/pages/component/MyHeader/MyHeader.js
@@ -0,0 +1,26 @@
+// pages/component/MyHeader/MyHeader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    myData: {
+      view: "自定义组件默认值",
+      type: String,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
